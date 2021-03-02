05675745eff609c00bb3b9e7fc22c8c20a554a14
Angel <garcialopez.ang@gmail.com>
2021-03-02 21:31:30 +0100

    props.children included in section

--- src/containers/Section.jsx
+++ src/containers/Section.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export function Section(){
+export function Section(props){
 
     return(
-        <section></section>
+        <section>{props.children}</section>
     );
 
 }
