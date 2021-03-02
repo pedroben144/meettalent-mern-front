543e777a18278fb38c13538957e811a98cd0cc39
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-03-02 18:50:48 +0100

    Added some styles for swiper and landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
 
 import MainWrapper from '../../containers/Main';
+import Slide1 from './components/Slide1';
+import Slide2 from './components/Slide2';
+import Slide3 from './components/Slide3';
+import Slide4 from './components/Slide4';
+
+SwiperCore.use([Pagination]);
 
 export default function LandingPage({...mainProps}) {
 
@@ -8,13 +18,33 @@ export default function LandingPage({...mainProps}) {
 
     mainProps[main] = true;
 
-    const toggleMain = () => {
-        main === 'blue' ? setMain('white') : setMain('blue');
+    // const toggleMain = (swiper) => {
+    //     main === 'blue' ? setMain('white') : setMain('blue');
+    //     console.log(swiper);
+    // }
+
+    const handleMainOnSwipe = (swiper) => {
+        if (swiper.realIndex !== 0) {
+            setMain('white');
+        } else {
+            setMain('blue');
+        }
     }
 
     return (
         <MainWrapper {...mainProps}>
-            <button onClick={toggleMain}>toggle bg</button>
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+                pagination
+                onSlideChange={(swiper) => handleMainOnSwipe(swiper)}
+                >
+                <SwiperSlide><Slide1 /></SwiperSlide>
+                <SwiperSlide><Slide2 /></SwiperSlide>
+                <SwiperSlide><Slide3 /></SwiperSlide>
+                <SwiperSlide><Slide4 /></SwiperSlide>
+            </Swiper>
+
         </MainWrapper>
     )
 }
---
+++ src/pages/LandingPage/components/Slide3.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Slide3() {
+    return (
+        <div className='swiper swiper--white'>Hello 3</div>
+    )
+}
