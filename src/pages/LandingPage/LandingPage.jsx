01ddc22c02e4a20783133487f42626569d4818ea
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-03-02 14:31:00 +0100

    Setted backgorund switcher on Landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import MainWrapper from '../../containers/Main';
 
-export default function LandingPage() {
+export default function LandingPage({...mainProps}) {
+
+    const [ main, setMain ] = useState('blue');
+
+    mainProps[main] = true;
+
+    const toggleMain = () => {
+        main === 'blue' ? setMain('white') : setMain('blue');
+    }
+
     return (
-        <div></div>
+        <MainWrapper {...mainProps}>
+            <button onClick={toggleMain}>toggle bg</button>
+        </MainWrapper>
     )
 }
