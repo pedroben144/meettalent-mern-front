50cdc760ca1fff67f9a9b0d535a4cc41ed135de8
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-03-04 23:24:45 +0100

    Finished Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { FiHome, FiUsers, FiBriefcase, FiSettings } from 'react-icons/fi';
+import { GoDiffAdded } from 'react-icons/go';
 
 export default function Footer() {
 
     return (
         <>
             <footer className="footer">
-
+                <NavLink className="footer__link" activeClassName="footer__link--active" to="/home"><FiHome /><span className="footer__link__text">Home</span></NavLink>
+                <NavLink className="footer__link" activeClassName="footer__link--active" to="/people"><FiUsers /><span className="footer__link__text">Candidatos</span></NavLink>
+                <NavLink className="footer__link" activeClassName="footer__link--active--create" to="/create"><GoDiffAdded /><span className="footer__link__text">Crear</span></NavLink>
+                <NavLink className="footer__link" activeClassName="footer__link--active" to="/jobs"><FiBriefcase /><span className="footer__link__text">Ofertas</span></NavLink>
+                <NavLink className="footer__link" activeClassName="footer__link--active" to="/perfil"><FiSettings /><span className="footer__link__text">Perfil</span></NavLink>
             </footer>
         </>
     )
