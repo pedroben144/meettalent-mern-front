2124ed99b76789d93d42e2b41b40a46ab8b89b57
Angel <garcialopez.ang@gmail.com>
2021-03-11 23:17:12 +0100

    candidateDetail finished

--- src/pages/PeoplePage/components/PeopleDetailPage.jsx
+++ src/pages/PeoplePage/components/PeopleDetailPage.jsx
@@ -1,61 +1,91 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../../containers/Header';
 import Section from '../../../containers/Section';
 import { MainContext } from '../../../contexts/MainContext';
 import { FaTwitter } from 'react-icons/fa';
 import { FiInstagram } from 'react-icons/fi';
 import { GrFacebookOption } from "react-icons/gr";
-import { AiOutlineCalendar } from "react-icons/ai";
+import { AiFillPauseCircle, AiOutlineCalendar } from "react-icons/ai";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { FiMail } from "react-icons/fi";
 import { FiPhone } from "react-icons/fi";
+import { useHistory, useParams } from 'react-router';
+import { API } from '../../../api/api.consts'
 
 
 export function PeopleDetailPage(props) {
 
+    let history = useHistory();
+
     const {setMainStyle} = useContext(MainContext);
     setMainStyle("blue");
 
+    const [candidate, setCandidate] = useState(null);
+    const{idPeople} = useParams();
+
+    const getCandidate = () => {
+        API.get("candidates/" + idPeople).then((res) => {
+            console.log(res.data.results);
+            setCandidate(res.data.results);
+        })
+    }
+
+    const goCandidates = () => {
+        history.push('/people');
+    }
+
+
+
+    useEffect(() => setMainStyle("blue"), []);
+    useEffect(getCandidate,[]);
     return (
         <>
-            <Header goBack className="header">Candidatos</Header>
+            
+            <Header goBack fn={goCandidates} >Candidatos</Header>
             <Section>
-            <div className="c-white-rectangle">
-                <img className="b-avatarImg  b-avatarImg--big b-avatarImg--absolute c-white-rectangle__image" src="/assets/images/logo-text.png" alt=""/>
-                <div className="c-white-rectangle__top">
-                    <h1 className="c-white-rectangle__top--h1">Fatima Ramos</h1>
-                    <h3 className="c-white-rectangle__top--h3">Diseñadora Web</h3>
-                    <FaTwitter className="c-white-rectangle__top--icons"/> 
-                    <FiInstagram className="c-white-rectangle__top--icons"/> 
-                    <GrFacebookOption className="c-white-rectangle__top--icons"/>
-                </div>
-                
-                <hr className="c-white-rectangle__rule"/>
-
-                <div className="c-white-rectangle__perosnal-data">
-                    <h4 className="c-white-rectangle__subtitles">Datos personales</h4>
-                    <div style={{"display":"flex"}}><AiOutlineCalendar className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">30 años</p></div>
-                    <div style={{"display":"flex"}}><HiOutlineLocationMarker className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">Madrid, capital, 28004</p></div>
-                    <div style={{"display":"flex"}}><FiMail className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">fatimaramos@gmail.com</p></div>
-                    <div style={{"display":"flex"}}><FiPhone className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">655-555-000</p></div>
-                </div>
-                
-                <div className="c-white-rectangle__key-words">
-                    <h4 className="c-white-rectangle__subtitles">Palabras clave del perfil</h4>
-                    <div className="c-white-rectangle__key-words--box">
-                        <p className="c-white-rectangle__key-words--box--text">UX/UI</p>
-                    </div>
-                </div>
-
-                <div className="c-white-rectangle__academic">
-                    <h4 className="c-white-rectangle__subtitles">Formación académica</h4>
-                    <div style={{"display":"flex"}}>
-                        <p className="c-white-rectangle__academic--degree">Master</p>
-                        <p className="c-white-rectangle__academic--career">Diseno web CEI: escuela de diseno</p>
-                    </div>
-                </div>
-                
+            {candidate && <div className="c-white-rectangle">
+            
+            <img className="b-avatarImg  b-avatarImg--big b-avatarImg--absolute c-white-rectangle__image" src={candidate.avatar} alt=""/>
+            <div className="c-white-rectangle__top">
+                <h1 className="c-white-rectangle__top--h1">{candidate.name} {candidate.surname}</h1>
+                <h3 className="c-white-rectangle__top--h3">{candidate.rol}</h3>
+                <FaTwitter className="c-white-rectangle__top--icons"/> 
+                <FiInstagram className="c-white-rectangle__top--icons"/> 
+                <GrFacebookOption className="c-white-rectangle__top--icons"/>
             </div>
+            
+            <hr className="c-white-rectangle__rule"/>
+
+            <div className="c-white-rectangle__perosnal-data">
+                <h4 className="c-white-rectangle__subtitles">Datos personales</h4>
+                <div className="c-white-rectangle__container-iconText"><AiOutlineCalendar className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">{candidate.age} años</p></div>
+                <div className="c-white-rectangle__container-iconText"><HiOutlineLocationMarker className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">{candidate.city}</p></div>
+                <div className="c-white-rectangle__container-iconText"><FiMail className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">{candidate.email}</p></div>
+                <div className="c-white-rectangle__container-iconText"><FiPhone className="c-white-rectangle__perosnal-data--icons"/><p className="c-white-rectangle__perosnal-data--text">{candidate.phone}</p></div>
+            </div>
+            
+            <div className="c-white-rectangle__key-words">
+                <h4 className="c-white-rectangle__subtitles">Palabras clave del perfil</h4>
+                <div className="c-white-rectangle__key-words--box">
+                    <p className="c-white-rectangle__key-words--box--text" style={candidate.creativity ? {"backgroundColor": "#00b3cd"} : {"backgroundColor": "#d8eef8","color":"#0b4f6e"}}>Creatividad</p>
+                    <p className="c-white-rectangle__key-words--box--text" style={candidate.uxui ? {"backgroundColor": "#00b3cd"} : {"backgroundColor": "#d8eef8","color":"#0b4f6e"}}>UX/UI</p>
+                    <p className="c-white-rectangle__key-words--box--text" style={candidate.sketch ? {"backgroundColor": "#00b3cd"} : {"backgroundColor": "#d8eef8","color":"#0b4f6e"}}>Sketch</p>
+                    <p className="c-white-rectangle__key-words--box--text" style={candidate.projects ? {"backgroundColor": "#00b3cd"} : {"backgroundColor": "#d8eef8","color":"#0b4f6e"}}>Proyectos</p>
+                    <p className="c-white-rectangle__key-words--box--text" style={candidate.empathy ? {"backgroundColor": "#00b3cd"} : {"backgroundColor": "#d8eef8","color":"#0b4f6e"}}>Empatía</p>
+                    <p className="c-white-rectangle__key-words--box--text" style={candidate.html ? {"backgroundColor": "#00b3cd"} : {"backgroundColor": "#d8eef8","color":"#0b4f6e"}}>HTML</p>
+                </div>
+            </div>
+
+            <div className="c-white-rectangle__academic">
+                <h4 className="c-white-rectangle__subtitles">Formación académica</h4>
+                <div className="c-white-rectangle__container-iconText">
+                    <p className="c-white-rectangle__academic--degree">Estudios:</p>
+                    <p className="c-white-rectangle__academic--career">{candidate.studies}</p>
+                </div>
+            </div>
+            
+        </div> }
+            
             </Section>
         </>
     );
